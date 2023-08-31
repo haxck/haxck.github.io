c8c45b5f2a900d5c9ce6686872603bb76fe942c0
Zhiqiang Du <haxck@qq.com>
2023-08-31 10:34:24 +0800

    updata to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://blog.haxck.com',
 	integrations: [mdx(), sitemap()],
-	experimental: {
-    		viewTransitions: true,
-	},
 });
