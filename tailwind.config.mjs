8d5eedfc9c9903ccbe313edd543fb4df3a7a31f7
Zhiqiang Du <haxck@qq.com>
2024-04-12 15:16:20 +0800

    adding dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,5 +10,6 @@ function withOpacity(variableName) {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   plugins: [require("@tailwindcss/typography")],
 };
