4590e318549514af7305d4b2be3bb1a8fc734f94
Zhiqiang Du <haxck@qq.com>
2024-04-17 16:58:11 +0800

    styles: use deyihei font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,6 @@
-function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
-}
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [],
 };
