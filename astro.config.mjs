1580776c5a431f67337a872e50b98e9f31701902
Zhiqiang Du <haxck@qq.com>
2024-11-28 11:10:22 +0800

    删除首页

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.haxck.com',
+  redirects: {
+    '/': '/posts/'
+  },
   integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,6 @@ export const SITE_TITLE = "Haxck's blog";
 export const SITE_DESCRIPTION = "Haxck's blog";
 
 export const NAVS = [
-    {"name":"首页","link":"/"},
     {"name":"博客","link":"/posts"},
     {"name":"收集","link":"/quote"},
     {"name":"清单","link":"/list"},
