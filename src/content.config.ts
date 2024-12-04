acdee8a5a5ba7e9ecbb0ede2d57a90f64003d3ff
Zhiqiang Du <haxck@qq.com>
2024-12-04 13:31:39 +0800

    updates astro5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
+	loader: glob({ pattern: '**\/[^_]*.md*', base: "./src/data/blog/" }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
